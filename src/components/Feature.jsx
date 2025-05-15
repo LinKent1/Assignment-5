2a571b64dd0aebd46ebe1ae75b6533b9b7670d5b
Lin <Kent.Lin@student.tdsb.on.ca>
2025-05-15 14:31:43 -0400

    fixed random

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -2,12 +2,25 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import MovieCard from './MovieCard'
 
+
+const shuffleArray = (array) => {
+  const shuffledArray = [...array];
+  for (let i = shuffledArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+  }
+  return shuffledArray;
+}
+
 function Feature() {
   const [movies, setMovies] = useState([])
 
   useEffect(() => {
     axios.get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${import.meta.env.VITE_TMDB_KEY}`)
-      .then(res => setMovies(res.data.results.slice(0, 3)))
+      .then(res => {
+        const shuffledMovies = shuffleArray(res.data.results);
+        setMovies(shuffledMovies.slice(0, 3)); 
+      })
   }, [])
 
   return (
